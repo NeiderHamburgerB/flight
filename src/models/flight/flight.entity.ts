9b873ad3896e1b0275dd74c1878b4f323ac49aa5
NeiderHamburgerB <neiderblanco99@gmail.com>
2023-10-30 15:54:05 -0500

    style

--- src/models/flight/flight.entity.ts
+++ src/models/flight/flight.entity.ts
@@ -1,5 +1,5 @@
-import { ApiProperty, ApiResponseProperty } from "@nestjs/swagger";
-import { 
+import { ApiProperty } from "@nestjs/swagger";
+import {
     Column,
     CreateDateColumn,
     Entity,
@@ -12,7 +12,7 @@ import { City } from "../city/city.entity";
 
 @Entity({ name: 'flights' })
 export class Flights {
-    
+
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
@@ -20,7 +20,7 @@ export class Flights {
     @ManyToOne(() => City, (city) => city.id)
     @JoinColumn({ name: 'origin_city_id' })
     origin_city?: City;
-  
+
     @ApiProperty({ description: 'Id de la ciudad de origen' })
     @Column({ type: 'int' })
     origin_city_id: number;
@@ -29,7 +29,7 @@ export class Flights {
     @ManyToOne(() => City, (city) => city.id)
     @JoinColumn({ name: 'destination_city_id' })
     destination_city?: City;
-  
+
     @ApiProperty({ description: 'Id de la ciudad de destino' })
     @Column({ type: 'int' })
     destination_city_id: number;
@@ -41,14 +41,14 @@ export class Flights {
     @ApiProperty({ description: 'Clase de servicio' })
     @Column({ type: 'varchar' })
     service_class: string;
-    
+
     @ApiProperty({ description: 'Tipo de avión' })
     @Column({ type: 'varchar' })
     aircraft_type: string;
 
     @ApiProperty({ description: 'Número de vuelo' })
     @Column({ type: 'varchar' })
-    flight_number:string
+    flight_number: string
 
     @ApiProperty({
         description: 'Fecha y Hora de inicio del vuelo de ida',
@@ -59,18 +59,18 @@ export class Flights {
     @ApiProperty({
         description: 'Fecha y Hora de inicio del vuelo de vuelta',
     })
-    @Column({ type: 'timestamptz', nullable:true })
+    @Column({ type: 'timestamptz', nullable: true })
     return_date_time: Date;
 
     @Column({ type: 'boolean' })
-    round_trip:boolean
+    round_trip: boolean
 
     @ApiProperty({
         description: 'Total pagado',
     })
     @Column({ type: 'decimal', precision: 10, scale: 2 })
     total_amount: number;
-    
+
     @CreateDateColumn({
         name: 'created_at',
         type: 'timestamptz',
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FlightModule } from './flight/flight.module';
 import { CityModule } from './city/city.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [CityModule, FlightModule ],
+  imports: [CityModule, FlightModule, ReservationModule ],
   controllers: [],
 })
 export class ApiModule { }
